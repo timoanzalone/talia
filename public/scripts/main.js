5e4a1e3a2754cda874f722b2f21cb91443f8e36c
timoanzalone <timo.anzalone@gmail.com>
2019-04-26 17:07:27 +0200

    added cookie notice

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,26 +1,3 @@
-new WOW().init()
-// var rellax = new Rellax('.rellax');
-// window.addEventListener("load", function(){
-//   window.cookieconsent.initialise({
-//     "palette": {
-//       "popup": {
-//         "background": "#ffffff",
-//         "text": "#000000",
-//       },
-//       "button": {
-//         "background": "#ffffff",
-//         "text": "#000000"
-//       }
-//     },
-//     "content": {
-//       "message": "Questo sito utilizza cookie tecnici per facilitare la navigazione, cookie di analytics per raccogliere dati in forma aggregata, e cookie di terze parti per poter offrire ulteriori servizi e funzionalità agli utenti. Cliccando sull’apposito pulsante, chiudendo questo banner, scorrendo questa pagina, cliccando su un link o proseguendo la navigazione in altra maniera, acconsenti all’uso dei cookie. Se vuoi saperne di più o negare il consenso a tutti o ad alcuni cookie,",
-//       "dismiss": "<span style='text-decoration: underline;'>Acconsento</span>",
-//       "link": "consulta la cookie policy",
-//       "href": "/privacy.html"
-//     }
-//   });
-// });
-
 $(document).ready(function() {
   $(".navbar-toggler").click(function() {
       $("#MobileNav").toggleClass("overlay");
@@ -29,9 +6,3 @@ $(document).ready(function() {
     $("#MobileNav").toggleClass("overlay");
   });
 });
-
-
-
-
-
-
