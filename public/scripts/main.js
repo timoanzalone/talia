759df399392bf242d4ca0cdf220bb985b24cc75b
timoanzalone <timo.anzalone@gmail.com>
2019-04-26 16:02:59 +0200

    update

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -25,6 +25,9 @@ $(document).ready(function() {
   $(".navbar-toggler").click(function() {
       $("#MobileNav").toggleClass("overlay");
   });
+  $("#MobileNav .nav-link").click(function() {
+    $("#MobileNav").toggleClass("overlay");
+  });
 });
 
 
